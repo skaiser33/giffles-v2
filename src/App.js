daa3f4f5e7ac5ef271f2297ea2981da0401c3576
skaiser33 <skaiser33@gmail.com>
2022-04-09 16:15:00 -0400

    builds app skeleton

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 // import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Giffles</h1>
+    <div className="container">
+      <Header />
     </div>
   );
 }
