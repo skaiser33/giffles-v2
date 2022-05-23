3a35331c6255a210f13fb28e3ff714c01d93ffd2
skaiser33 <skaiser33@gmail.com>
2022-05-23 19:16:41 -0400

    use map to populate gifs and promise.all for api requests

--- src/components/GifflesContainer.js
+++ src/components/GifflesContainer.js
@@ -5,18 +5,21 @@ import axios from 'axios';
 import GifContainer from './GifContainer';
 import getTitle from '../api/getTitle';
 
-
 const GifflesContainer = () => {
 
   const [title, setTitle] = useState("");
   const [gifWords, setGifWords] = useState([]);
-  const [gif1, setGif1] = useState("about:blank")
-  const [gif2, setGif2] = useState("about:blank")
-  const [gif3, setGif3] = useState("about:blank")
-  const [gif4, setGif4] = useState("about:blank")
+  const [gifSources, setGifSources] = useState(["about:blank", "about:blank", "about:blank"])
 
-  // let gifWords = [];
-
+  const gifContainers = gifSources.map((gifSource, idx) => {
+    console.log(gifSource);
+    return (
+      <>
+        <GifContainer key={gifSource} id={(idx + 1).toString()} title={(idx + 1).toString()} gif={gifSource}/>
+        <p id={`pre${(idx + 2).toString()}`}></p>
+      </>
+    )
+  })
 
   const loadGifs = () => {
     setGifWords(getTitle());
@@ -30,37 +33,50 @@ const GifflesContainer = () => {
     //   }
     //   getGifWords();
     // }
-    console.log('GIFWORDS where i need it ---->' + gifWords );
+    console.log('GIFWORDS ---->' + gifWords );
   }
 
   useEffect(() => {
     const getGifs = async () => {
       try {
         // CREATE CONTROL IF GIFWORDS IS EMPTY
-        const res1 = await axios.get(
-          `https://api.giphy.com/v1/gifs/search?api_key=${process.env.REACT_APP_GIPHY_API_KEY}&q=${gifWords[0]}&limit=5&offset=0&lang=en`
-        );
-        const res1Gifs = await res1.data;
-        // console.log(resGifs.data[0]);
-        // setGif1(() => resGifs.data[0].embed_url);
-        setGif1(() => res1Gifs.data[0].images.fixed_height.url);
-        // console.log(resGifs.data[0].images.fixed_height.url);
-        const res2 = await axios.get(
-          `https://api.giphy.com/v1/gifs/search?api_key=${process.env.REACT_APP_GIPHY_API_KEY}&q=${gifWords[1]}&limit=5&offset=0&lang=en`
-        );
-        const res2Gifs = await res2.data;
-        // console.log(resGifs.data[0]);
-        // setGif1(() => resGifs.data[0].embed_url);
-        setGif2(() => res2Gifs.data[0].images.fixed_height.url);
-        // console.log(resGifs.data[0].images.fixed_height.url);
-        const res3 = await axios.get(
-          `https://api.giphy.com/v1/gifs/search?api_key=${process.env.REACT_APP_GIPHY_API_KEY}&q=${gifWords[2]}&limit=5&offset=0&lang=en`
-        );
-        const res3Gifs = await res3.data;
-        // console.log(resGifs.data[0]);
-        // setGif1(() => resGifs.data[0].embed_url);
-        setGif3(() => res3Gifs.data[0].images.fixed_height.url);
-        // console.log(resGifs.data[0].images.fixed_height.url);
+        const gifUrls = gifWords.map(async(word) => {
+          const res = await axios.get(
+                  `https://api.giphy.com/v1/gifs/search?api_key=${process.env.REACT_APP_GIPHY_API_KEY}&q=${word}&limit=5&offset=0&lang=en`
+                );
+                const resGifs = await res.data;
+                // console.log(resGifs.data[0]);
+                // setGif1(() => resGifs.data[0].embed_url);
+                // setGif1(() => res1Gifs.data[0].images.fixed_height.url);
+                return resGifs.data[0].images.fixed_height.url;
+        })        
+        
+        // console.log(await Promise.all(gifUrls));
+        setGifSources(await Promise.all(gifUrls));
+        // const res1 = await axios.get(
+        //   `https://api.giphy.com/v1/gifs/search?api_key=${process.env.REACT_APP_GIPHY_API_KEY}&q=${gifWords[0]}&limit=5&offset=0&lang=en`
+        // );
+        // const res1Gifs = await res1.data;
+        // // console.log(resGifs.data[0]);
+        // // setGif1(() => resGifs.data[0].embed_url);
+        // setGif1(() => res1Gifs.data[0].images.fixed_height.url);
+        // // console.log(resGifs.data[0].images.fixed_height.url);
+        // const res2 = await axios.get(
+        //   `https://api.giphy.com/v1/gifs/search?api_key=${process.env.REACT_APP_GIPHY_API_KEY}&q=${gifWords[1]}&limit=5&offset=0&lang=en`
+        // );
+        // const res2Gifs = await res2.data;
+        // // console.log(resGifs.data[0]);
+        // // setGif1(() => resGifs.data[0].embed_url);
+        // setGif2(() => res2Gifs.data[0].images.fixed_height.url);
+        // // console.log(resGifs.data[0].images.fixed_height.url);
+        // const res3 = await axios.get(
+        //   `https://api.giphy.com/v1/gifs/search?api_key=${process.env.REACT_APP_GIPHY_API_KEY}&q=${gifWords[2]}&limit=5&offset=0&lang=en`
+        // );
+        // const res3Gifs = await res3.data;
+        // // console.log(resGifs.data[0]);
+        // // setGif1(() => resGifs.data[0].embed_url);
+        // setGif3(() => res3Gifs.data[0].images.fixed_height.url);
+        // // console.log(resGifs.data[0].images.fixed_height.url);
 
       } catch (error) {
         console.log(error);
@@ -71,10 +87,7 @@ const GifflesContainer = () => {
     // gifWords = getTitle();
     // console.log('GIFWORDS where i need it ---->' + gifWords);
     
-      //If title length is too short / too long, pick another title
-    // while (gifWords.length < 2 || gifWords.length > 4) {
-    //   gifWords = getTitle();
-    // }   
+
       //resize iframes if needed
       //***TO BE UNCOMMENTED??*** (gifWords.length === 4) ? shrinkGifs() : enlargeGifs();   
   }, [gifWords]);
@@ -83,15 +96,16 @@ const GifflesContainer = () => {
     // <h3>sanity check</h3>
     <div className="giffles-container">
       <p id="pre1"></p>
+      {gifContainers}
       {/* <GifContainer id="1" title="one" word={gifWords[0] ? gifWords[0] : "Loading"}/> */}
-      <GifContainer id="1" title="one" gif={gif1}/>
+      {/* <GifContainer id="1" title="one" gif={gifSources[0]}/>
       <p id="pre2"></p>
-      <GifContainer id="2" title="two" gif={gif2}/>
+      <GifContainer id="2" title="two" gif={gifSources[1]}/>
       <p id="pre3"></p>
-      <GifContainer id="3" title="three" gif={gif3}/>
+      <GifContainer id="3" title="three" gif={gifSources[2]}/>
       <p id="pre4"></p>
-      {/* <GifContainer id="4" title="four" word={gifWords[0]}/> */}
-      <p id="pre5"></p>
+      <GifContainer id="4" title="four" word={gifSources[3]}/>
+      <p id="pre5"></p> */}
       <button onClick={loadGifs}>TESTER</button>
     </div>  
   );
