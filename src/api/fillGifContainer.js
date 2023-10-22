9e4217edaa2afc780805593ff10d3c2da3755b20
skaiser33 <skaiser33@gmail.com>
2023-10-22 14:17:57 -0400

    moves fillGifContainer function to separate file

---
+++ src/api/fillGifContainer.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import GifContainer from '../components/clues/GifContainer';
+
+const fillGifContainer = (title, gifSources) => {
+  return gifSources.map((gifSource, idx) => {
+    console.log('gifSource in fill', gifSource);
+    return (
+      <>
+        <GifContainer
+          key={gifSource.url}
+          id={(idx + 1).toString()}
+          title={(idx + 1).toString()}
+          gif={gifSource}
+        />
+        {/* <p id={`pre${(idx + 2).toString()}`}></p> */}
+        {title.nonGifText[idx + 2] ? (
+          <p
+            key={`pre${(idx + 2).toString()}`}
+            id={`pre${(idx + 2).toString()}`}
+          >
+            {title.nonGifText[idx + 2]}
+          </p>
+        ) : (
+          <></>
+        )}
+      </>
+    );
+  });
+};
+//
+export default fillGifContainer;
--- src/components/clues/CluesContainer.js
+++ src/components/clues/CluesContainer.js
@@ -3,99 +3,80 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import GifContainer from './GifContainer';
+import fillGifContainer from '../../api/fillGifContainer';
 import EmptyGifContainer from './EmptyGifContainer';
 import getTitle from '../../api/getTitle';
 import clearAll from '../../api/clearAll';
 
 const CluesContainer = ({ selectedCategory, gifCounter }) => {
-  console.log('gifCounter in CluesContainer', gifCounter);
+  // console.log('gifCounter in CluesContainer', gifCounter);
   const [title, setTitle] = useState({
     titleString: [],
     gifWords: [],
     nonGifText: {},
   });
   // const [gifSources, setGifSources] = useState(["#", "#", "#", "#"])
+  // const [gifData, setGifData] = useState([]);
   const [gifSources, setGifSources] = useState([]);
 
-  useEffect(() => {
-    // *** MUST AVOID CALLING USEEFFECT ON INITIAL RENDER -- RIGHT?
-    // https://stackoverflow.com/questions/53179075/with-useeffect-how-can-i-skip-applying-an-effect-upon-the-initial-render
-    // https://typeofnan.dev/how-to-prevent-useeffect-from-running-on-mount-in-react/
-    console.log('useEffect Called, gifCounter is', gifCounter);
-  }, [gifCounter]);
-
-  const fillGifContainers = gifSources.map((gifSource, idx) => {
-    // console.log(gifSource);
-    return (
-      <>
-        <GifContainer
-          key={gifSource.url}
-          id={(idx + 1).toString()}
-          title={(idx + 1).toString()}
-          gif={gifSource}
-        />
-        {/* <p id={`pre${(idx + 2).toString()}`}></p> */}
-        {title.nonGifText[idx + 2] ? (
-          <p
-            key={`pre${(idx + 2).toString()}`}
-            id={`pre${(idx + 2).toString()}`}
-          >
-            {title.nonGifText[idx + 2]}
-          </p>
-        ) : (
-          <></>
-        )}
-      </>
-    );
-  });
+  // useEffect(() => {
+  //   // *** MUST AVOID CALLING USEEFFECT ON INITIAL RENDER -- RIGHT?
+  //   // https://stackoverflow.com/questions/53179075/with-useeffect-how-can-i-skip-applying-an-effect-upon-the-initial-render
+  //   // https://typeofnan.dev/how-to-prevent-useeffect-from-running-on-mount-in-react/
+  //   console.log('useEffect Called, gifCounter is', gifCounter);
+  // }, [gifCounter]);
 
   const loadGifs = () => {
     if (gifSources.length) {
       clearAll(gifSources.length);
     }
     setTitle(getTitle(selectedCategory));
-    // console.log("i'm alive");
-    // const gettitle = async () => {
-    //   try {
-    //     const res = await getTitle();
-    //     console.log('res --->' + res);
-    //   } catch (error) {
-    //     console.log(error);
-    //   }
-    //   gettitle();
-    // }
   };
 
   useEffect(() => {
     const getGifs = async () => {
       try {
         // CREATE CONTROL IF title IS EMPTY
+        // const gifDataArray = title.gifWords.map(async (word) => {
+        //   const res = await axios.get(
+        //     `https://api.giphy.com/v1/gifs/search?api_key=${process.env.REACT_APP_GIPHY_API_KEY}&q=${word}&limit=5&offset=0&lang=en`
+        //   );
+        //   const resGifs = await res.data;
+        //   // console.log(resGifs.data[0]);
+        //   // fixed height of 200px; width is variable
+        //   return resGifs.data;
+        // });
+        // console.log('gifDataArray', await Promise.all(gifDataArray));
+        // setGifData(await Promise.all(gifDataArray));
+        // console.log('gifData1', gifData);
+        // // if (gifData[0] && gifData[0].images) {
+        // //   setGifSources(gifData[0].images.fixed_height);
+        // // }
+        // ***HOW IT USED TO WORK***
         const gifUrls = title.gifWords.map(async (word) => {
           const res = await axios.get(
             `https://api.giphy.com/v1/gifs/search?api_key=${process.env.REACT_APP_GIPHY_API_KEY}&q=${word}&limit=5&offset=0&lang=en`
           );
           const resGifs = await res.data;
           console.log(resGifs.data);
-          // setGif1(() => resGifs.data[0].embed_url);
-          // setGif1(() => res1Gifs.data[0].images.fixed_height.url);
-          // console.log('width-->' + resGifs.data[0].images.fixed_height.width);
           // fixed height of 200px; width is variable
           return resGifs.data[0].images.fixed_height;
         });
+        console.log('gifUrls', gifUrls);
         setGifSources(await Promise.all(gifUrls));
-        // console.log('TITLE ---->' + title.titleString);
-        // console.log('GIFWORDS ---->' + title.gifWords);
-        // console.log('NONGIFTEXT OBJ --->' + title.nonGifText);
+        console.log('NONGIFTEXT OBJ --->' + title.nonGifText);
+        // ***END OF HOW IT USED TO WORK***
       } catch (error) {
         console.log(error);
       }
     };
     getGifs();
-    //resize gifContainer if needed
+    //***resize gifContainer if needed ***
     //***TO BE UNCOMMENTED??*** (title.gifWords.length === 4) ? shrinkGifs() : enlargeGifs();
   }, [title]);
+  // console.log('gifData2', gifData);
+  // console.log('gifSources', gifSources);
   return (
-    // <h3>sanity check</h3>
     <>
       <div className='clues-container'>
         {/* <p id="pre1"></p>*/}
@@ -104,7 +85,12 @@ const CluesContainer = ({ selectedCategory, gifCounter }) => {
         ) : (
           <></>
         )}
-        {gifSources.length ? fillGifContainers : <EmptyGifContainer />}
+        {/* {gifSources.length ? fillGifContainers : <EmptyGifContainer />} */}
+        {gifSources.length ? (
+          fillGifContainer(title, gifSources)
+        ) : (
+          <EmptyGifContainer />
+        )}
         {/* <GifContainer id="1" title="one" word={title.titleString ? title.titleString : "Loading"}/> */}
       </div>
       <button onClick={loadGifs}>TESTER</button>
